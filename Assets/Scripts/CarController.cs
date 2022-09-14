73040e0d7e55626cfb16870e70aa29d4c8d7611f
KeMichelle <michellebellissima56@gmail.com>
2022-09-14 18:15:31 +0200

    script per muovere la macchina, non finito del tutto ma funzionante
    abbiamo usato il manuale unity e guardato alcuni esempi per sviluppare un semplice programmino che fa muovere la macchina.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backLeft;
 
-    // Update is called once per frame
-    void Update()
+    //variables
+    public float acceleration = 600f;
+    public float breakingForce = 300f;
+    public float maxTurnAngle = 18f;
+
+    private float currentAcceleration;
+    private float currentBreak;
+    private float currentTurnAngle;
+
+    private void FixedUpdate()
     {
-        
+        //foward/reverse from the w and s keys. from the vertical axes (which is usually up or down)
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+            currentBreak = breakingForce;
+        else
+            currentBreak = 0;
+
+        //apply acceleration only to the front wheels
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        //brake torque is essentially the power of the braking system. 
+        frontRight.brakeTorque = currentBreak;
+        frontLeft.brakeTorque = currentBreak;
+        backRight.brakeTorque = currentBreak;
+        backLeft.brakeTorque = currentBreak;
+
+        //steering left/right with "a" and "d" keys. uses the x axis
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
     }
 }
+
